683d00c5cf4cafe8a59f0b357bc1e2d12435ee26
marcelbroedel <marcelbroedel@gmx.de>
2026-02-17 15:03:33 +0100

    Remove command to empty result folder

--- run_example.sh
+++ run_example.sh
@@ -8,10 +8,6 @@ cd "$(dirname "$0")" || exit 1
 
 CONFIG="src/flex_dep_opt/config/settings_example.yaml"
 
-# --- Empty result folder completely ---
-# Remove read-only attribute isn't needed on Linux
-rm -rf results/*
-
 # --- Run the simulations ---
 python -m flex_dep_opt run-sim --config "$CONFIG"
 python -m flex_dep_opt run-post --config "$CONFIG"
