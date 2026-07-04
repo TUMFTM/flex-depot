ac1c4fbae3ed92065d6a7c2fc1f74a8998e38cf6
marcelbroedel <marcelbroedel@gmx.de>
2026-07-04 20:11:54 +0200

    updated market comparison, 150kW, 48h ID

--- paper/batches/plot_market_comparison.bat
+++ paper/batches/plot_market_comparison.bat
@@ -2,5 +2,5 @@
 setlocal
 cd /d "%~dp0\..\.."
 python paper\scripts\market_comparison\plot_market_comparison.py ^
-  --manifest results\batches\market_comparison__2026-07-04_10-26-16\manifest.csv ^
+  --manifest results\batches\market_comparison__2026-07-04_17-40-07\manifest.csv ^
   --output-base paper\figures\market_comparison\market_comparison_profit_delta
