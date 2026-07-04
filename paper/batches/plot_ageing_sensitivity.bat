0176cb51ce131ee6abab0c74def606a604a1d976
marcelbroedel <marcelbroedel@gmx.de>
2026-07-04 14:59:52 +0200

    ageing sens plot

--- paper/batches/plot_ageing_sensitivity.bat
+++ paper/batches/plot_ageing_sensitivity.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0\..\.."
-rem TODO: replace REPLACE_ME with the actual batch run timestamp once results exist
 python paper\scripts\ageing_sensitivity\plot_ageing_sensitivity.py ^
-  --manifest results\batches\ageing_sensitivity__REPLACE_ME\manifest.csv ^
+  --manifest results\batches\ageing_sensitivity__2026-07-04_04-34-49_idhorizon1h\manifest.csv ^
   --output-base paper\figures\ageing_sensitivity\ageing_sensitivity
