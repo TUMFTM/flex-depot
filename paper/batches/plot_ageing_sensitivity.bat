2a68a877dd908a21e41eca8642b146330c0c2a39
marcelbroedel <marcelbroedel@gmx.de>
2026-07-05 10:38:52 +0200

    ageing plots

--- paper/batches/plot_ageing_sensitivity.bat
+++ paper/batches/plot_ageing_sensitivity.bat
@@ -2,5 +2,5 @@
 setlocal
 cd /d "%~dp0\..\.."
 python paper\scripts\ageing_sensitivity\plot_ageing_sensitivity.py ^
-  --manifest results\batches\ageing_sensitivity__2026-07-04_04-34-49_idhorizon1h\manifest.csv ^
+  --manifest results\batches\ageing_sensitivity__2026-07-04_23-03-01\manifest.csv ^
   --output-base paper\figures\ageing_sensitivity\ageing_sensitivity
